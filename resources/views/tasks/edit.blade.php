39ad6b9bed45e3bcf7f72b8cec709b7810d9a9f5
yasuaki.watanabe <lapdog717@nifty.com>
2017-07-22 12:12:02 +0000

    bootstrap2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,10 +6,8 @@
 
 
 <div class="row">
-        <div class="col-xs-12">
-        <div class="col-sm-offset-2 col-sm-8">
-        <div class="col-md-offset-2 col-md-8">
-        <div class="col-lg-offset-3 col-lg-6">
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
@@ -24,9 +22,6 @@
                 {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
             {!! Form::close() !!}
-        </div>
-        </div>
-        </div>
         </div>
 </div>
 @endsection
