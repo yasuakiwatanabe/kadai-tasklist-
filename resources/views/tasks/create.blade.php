6cdbf1851377fbedf02f9c4a82213a1718dadf0a
yasuaki.watanabe <lapdog717@nifty.com>
2017-07-22 07:41:09 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,18 +3,30 @@
 @section('content')
 
 <h1>タスク新規作成ページ</h1>
-
+<div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class="col-md-offset-2 col-md-8">
+        <div class="col-lg-offset-3 col-lg-6">
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('登録') !!}
-
-    {!! Form::close() !!}
-
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('登録', ['class' => 'btn btn-primary']) !!}
+        
+            {!! Form::close() !!}
+    </div>
+    </div>
+    </div>
+    </div>
+</div>
 @endsection
